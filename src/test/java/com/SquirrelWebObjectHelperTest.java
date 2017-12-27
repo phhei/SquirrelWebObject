5ee9c333a613bf96767b5bd84b28962d1384d070
Philipp Heinisch <philipp.heinisch@gmail.com>
2017-12-27 09:02:36 +0100

    Improve Test -> new version

--- src/test/java/com/SquirrelWebObjectHelperTest.java
+++ src/test/java/com/SquirrelWebObjectHelperTest.java
@@ -2,16 +2,24 @@ package com;
 
 import org.junit.jupiter.api.Assertions;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
 
 class SquirrelWebObjectHelperTest {
 
     @org.junit.jupiter.api.Test
     void convertToObject() {
         SquirrelWebObject o = new SquirrelWebObject();
+        List<String> pendingURIlist = new ArrayList<>(3);
+        pendingURIlist.add("1. URI");
+        pendingURIlist.add("https://philippheinisch.de");
+        pendingURIlist.add("<http://dbPedia.org/ontology/>");
+        o.setPendingURIs(pendingURIlist);
+
         byte[] stream = o.convertToByteStream();
 
         SquirrelWebObject cal = SquirrelWebObjectHelper.convertToObject(stream);
+
         Assertions.assertEquals(o.getCountOfPendingURIs(), cal.getCountOfPendingURIs());
     }
 }
