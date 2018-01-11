8571b78a7993fa2bb7edeae278d79ca478a4ecc7
Philipp Heinisch <philipp.heinisch@gmail.com>
2018-01-11 16:43:33 +0100

    Overwrite equals method + improve test

--- src/test/java/com/SquirrelWebObjectTest.java
+++ src/test/java/com/SquirrelWebObjectTest.java
@@ -1,25 +1,33 @@
 package com;
 
-import org.junit.jupiter.api.Assertions;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
-class SquirrelWebObjectHelperTest {
+import static org.junit.Assert.*;
 
-    @org.junit.jupiter.api.Test
-    void convertToObject() {
-        SquirrelWebObject o = new SquirrelWebObject();
+public class SquirrelWebObjectTest {
+
+    SquirrelWebObject o;
+
+    @Before
+    public void setUp() throws Exception {
+        o = new SquirrelWebObject();
         List<String> pendingURIlist = new ArrayList<>(3);
         pendingURIlist.add("1. URI");
         pendingURIlist.add("https://philippheinisch.de");
         pendingURIlist.add("<http://dbPedia.org/ontology/>");
         o.setPendingURIs(pendingURIlist);
+    }
 
+    @Test
+    public void convertToByteStream() {
         byte[] stream = o.convertToByteStream();
 
         SquirrelWebObject cal = SquirrelWebObjectHelper.convertToObject(stream);
 
-        Assertions.assertEquals(o.getCountOfPendingURIs(), cal.getCountOfPendingURIs());
+        assertEquals(o.getCountOfPendingURIs(), cal.getCountOfPendingURIs());
     }
 }
